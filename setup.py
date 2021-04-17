663fc24a320fb99214e4474e9ad46ebb1d00abb6
keith <keith.woolterton@gmail.com>
2021-04-17 09:20:17 +0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Nimrod',
+    version='1.0.0.0',
+    packages=['view', 'audio', 'model', 'utils', 'utils.trpg', 'controller'],
+    url='',
+    license='',
+    author='keith',
+    author_email='',
+    description='2.5D pygame',
+    install_requires=['pygame']
+)
